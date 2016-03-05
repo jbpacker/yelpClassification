99d285e7982b42f0859b7d951653cba74b0931ff
Jef Packer <jbpacker@ucdavis.edu>
2016-03-04 20:01:47 -0800

    works from new folder

--- secondaryFunctions/liteData.py
+++ secondaryFunctions/liteData.py
@@ -3,15 +3,15 @@ import csv
 
 #need to make directory /data/lite/train_photos
 dataLimitCnt = 0
-with open('data/train.csv', 'rb') as trainData:
+with open('../data/train.csv', 'rb') as trainData:
     trainReader = csv.DictReader(trainData)
-    with open('data/train_photo_to_biz_ids.csv', 'rb') as idToPhoto:
+    with open('../data/train_photo_to_biz_ids.csv', 'rb') as idToPhoto:
         idToPhotoReader = csv.DictReader(idToPhoto)
-        with open('data/lite/train.csv', 'wb') as newTrain:
+        with open('../data/lite/train.csv', 'wb') as newTrain:
             trainFields = ['business_id', 'labels']
             trainWrite = csv.DictWriter(newTrain, delimiter=',', fieldnames=trainFields)
             trainWrite.writeheader()
-            with open('data/lite/train_photo_to_biz_ids.csv', 'wb') as newIdToPhoto:
+            with open('../data/lite/train_photo_to_biz_ids.csv', 'wb') as newIdToPhoto:
                 idToPhotoFields = ['photo_id', 'business_id']
                 idToPhotoWrite = csv.DictWriter(newIdToPhoto, delimiter=',', fieldnames=idToPhotoFields)
                 idToPhotoWrite.writeheader()
@@ -32,11 +32,11 @@ with open('data/train.csv', 'rb') as trainData:
                             if ID == photoRow['business_id']:
                                 photoId = photoRow['photo_id']
                                 idToPhotoWrite.writerow({'photo_id': photoId, 'business_id': ID})
-                                fromFile = 'data/train_photos/' + photoId + '.jpg'
-                                toFile = 'data/lite/train_photos/' + photoId + '.jpg'
+                                fromFile = '../data/train_photos/' + photoId + '.jpg'
+                                toFile = '../data/lite/train_photos/' + photoId + '.jpg'
                                 shutil.copyfile(fromFile, toFile)
-                                fromFile = 'data/train_photos/._' + photoId + '.jpg'
-                                toFile = 'data/lite/train_photos/._' + photoId + '.jpg'
+                                fromFile = '../data/train_photos/._' + photoId + '.jpg'
+                                toFile = '../data/lite/train_photos/._' + photoId + '.jpg'
                                 shutil.copyfile(fromFile, toFile)
                         idToPhoto.seek(0)
                     else:
