39bf40bca283c2375c105aaa0212eb8112c50fd4
jbpacker <jeffersonpacker@gmail.com>
2016-03-05 16:52:16 -0800

    baseline picture dimension reduction

--- reduce.m
+++ reduce.m
@@ -1,4 +1,8 @@
-clear all; close all; clc;
+clear all; close all; clc; clear mex;
+
+dataPath = './data/lite/';
+num_features = 1000;
+important_label = 1;
 
 %% setup steps
 %setup/compile neural network files
@@ -11,20 +15,28 @@ run(fullfile('matconvnet-1.0-beta18', 'matlab', 'vl_setupnn.m'));
 cnnModel.net = load('imagenet-vgg-f.mat');
 addpath('matconvnet-1.0-beta18');
 %these tell me which pictures are important 
-train = readtable('data/lite/train.csv');
-biz = readtable('data/lite/train_photo_to_biz_ids.csv');
+train = readtable([dataPath, 'train.csv']);
+biz = readtable([dataPath, 'train_photo_to_biz_ids.csv']);
 
-for b = 1:size(train,1)
+X = zeros(size(biz,1), 3+num_features);
+
+ for b = 1:size(train,1)
     photo_ids = biz.photo_id(find(biz.business_id == train.business_id(b)));
+    fprintf('business %5.0f: %2.0f of %2.0f\n', train.business_id(b), b, size(train,1));
     for i = 1:length(photo_ids)
 
-        img = imread(['data/train_photos/', num2str(photo_ids(i)), '.jpg']);
+        img = imread([dataPath, 'train_photos/', num2str(photo_ids(i)), '.jpg']);
 
-%         [featureVector, hogVisualization] = extractHOGFeatures(img, 'CellSize', [6 6]);
         [classLabel, scores, batchTime] = cnnPredict(cnnModel, img, 'display', false);
         
-        figure;
-        imshow(img); hold on;
-        X(i,:) = featureVector;
+        %for fun
+        fprintf('labeled as: %s\n', char(classLabel));
+        
+        %output format:
+        % <instance_name>,<bag_name>,<label>,<value> ... <value>
+        X(i,1) = photo_ids(i);
+        X(i,2) = train.business_id(b);
+        X(i,3) = ~isempty(strfind(char(train.labels(b)), num2str(important_label)));
+        X(i,4:end) = scores';
     end
 end
